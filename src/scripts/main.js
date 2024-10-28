46ff3ba0166f3cf6c44c2374d4e93e725c0f6870
Amadeo Bon <amadeosph@outlook.com>
2024-10-28 10:21:05 -0300

    Adicionando controle de volume

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,6 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
+const volumeSlider = document.querySelector(".volume-slider input");
+
 let mapedKeys = [];
 let audio = new Audio("/src/assets/tunes/a.wav");
 
@@ -23,3 +25,9 @@ document.addEventListener("keydown", (e) => {
     playTune(e.key);
   }
 });
+
+const handleVolume = (e) => {
+  audio.volume = e.target.value;
+};
+
+volumeSlider.addEventListener("input", handleVolume);
